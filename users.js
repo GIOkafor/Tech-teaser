e75062c7cc7d96b684543ac07161922620bae693
Ikechukwu Okafor <>
2021-04-12 10:25:03 +0000

    Initial commit

---
+++ users.js
@@ -0,0 +1,82 @@
+const users = [
+    {
+        "id": "1",
+        "name": "Miko",
+        "age": 20,
+        "activities": ["Biking", "Running", "Biking"],
+        "member_rating": 2,
+        "salary": 2500
+    },
+    {
+        "id": "2",
+        "name": "Jesse, Pinkman",
+        "age": 33,
+        "activities": ["Running", "Running", "Hiking"],
+        "member_rating": 3,
+        "salary": 2000
+    },
+    {
+        "id": "3",
+        "name": "Joanna",
+        "age": 67,
+        "activities": ["Hiking", "Hiking", "Biking"],
+        "member_rating": 5,
+        "salary": 1000
+    },
+    {
+        "id": "4",
+        "name": "Alex",
+        "age": 18,
+        "activities": ["Hiking", "Running", "Biking"],
+        "member_rating": 1,
+        "salary": 2000
+    },
+    {
+        "id": "5",
+        "name": "Merissa",
+        "age": 22,
+        "activities": ["Running", "Running", "Biking"],
+        "member_rating": 2,
+        "salary": 3000
+    },
+    {
+        "id": "6",
+        "name": "Hassan",
+        "age": 25,
+        "activities": ["Hiking", "Running", "Running"],
+        "member_rating": 4,
+        "salary": 2000
+    },
+    {
+        "id": "7",
+        "name": "Hannah",
+        "age": 19,
+        "activities": ["Biking", "Biking", "Hiking"],
+        "member_rating": 2,
+        "salary": 1000
+    },
+    {
+        "id": "8",
+        "name": "Tina",
+        "age": 38,
+        "activities": ["Hiking", "Hiking", "Hiking"],
+        "member_rating": 1,
+        "salary": 5000
+    },
+    {
+        "id": "9",
+        "name": "Mike",
+        "age": 34,
+        "activities": ["Running", "Running", "Biking"],
+        "member_rating": 3,
+        "salary": 7000
+    },
+    {
+        "id": "10",
+        "name": "Irwin",
+        "age": 101,
+        "activities": ["Biking", "Running", "Biking"],
+        "member_rating": 4,
+        "salary": 2000
+    }
+]
